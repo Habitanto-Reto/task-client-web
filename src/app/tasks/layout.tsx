b8c547788666ee3fa55cdb9e8238b3fbe30ee4b4
Santiago Gaona <sgaonacarvajal@outlook.com>
2024-01-22 01:52:47 -0500

    ✨feat!: implementation auth & tasks (#9)
    * ✨feat!: implementation #3 #4 #5 #6 #7 (#8)
    
    * 📦️feat(npm): add or update compiled files or packages
    
    * 🧱feat: infrastructure related changes.
    
    * ✨feat: auth datasource #3
    
    * ✨feat: implementation repository auth #4
    
    * 💄feat(ui): shadcn implementation
    
    * ✨feat: authentication system controller & register #5
    
    * ✨feat(ui): pages for authentication system #5
    
    * feat(ci): configuration for ci
    
    * feat(npm): updata package.json
    
    * feat: implementation domain
    
    * feat: implementation infrastructure
    
    * feat: server actions
    
    * feat: auth system
    
    * feat(ui): components shadcn
    
    * feat: task implementation system
    
    * feat: task layout page

---
+++ src/app/tasks/layout.tsx
@@ -0,0 +1,24 @@
+import {auth, signOut} from "@/auth.config";
+import {redirect} from "next/navigation";
+import {LogoutButton} from "@/components/sidebar/LogoutButton";
+import {Toaster} from "@/components/ui/toaster";
+import Navbar from "@/components/sidebar/Navbar";
+
+export default async function ShopLayout( { children }: {
+    children: React.ReactNode;
+} ) {
+    const session = await auth();
+
+    if ( !session?.user ) {
+        //redirect('/auth/login?returnTo=/tasks');
+        redirect('/auth/login');
+    }
+
+    return (
+        <main className="justify-center">
+            <Navbar/>
+            { children }
+            <Toaster />
+        </main>
+    );
+}
