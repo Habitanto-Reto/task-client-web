f8d4ac42866f8d5b8f2d1cf3e0300caf11c14f97
Santiago Gaona <sgaonacarvajal@outlook.com>
2024-01-22 18:34:29 -0500

    💄feat(ui): not-found page & refactor ui (#15)
    * 💄feat(ui): not-found page & refactor ui (#14)

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterForm } from './ui/RegisterForm';
 export default function NewAccountPage() {
     return (
         <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
-            <h1 className={ `${ titleFont.className } text-4xl mb-5 text-center` }>Nueva cuenta</h1>
+            <h1 className={ `${ titleFont.className } text-4xl mb-5 text-center` }>Sign Up</h1>
             <RegisterForm />
         </div>
     );
