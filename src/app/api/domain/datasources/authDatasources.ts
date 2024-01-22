dc713bde704fdd9c312da60656f0437f9bceb157
SantiagoGaonaC <sgaonacarvajal@outlook.com>
2024-01-22 01:45:57 -0500

    feat: implementation domain

--- src/app/api/domain/datasources/authDatasources.ts
+++ src/app/api/domain/datasources/authDatasources.ts
@@ -3,4 +3,5 @@ import {IUserLoginResponse, IUserResponse} from "@/app/api/domain/entities/authR
 export abstract class AuthDatasources {
     abstract registerUser(name: string, email: string, password: string): Promise<IUserResponse>;
     abstract loginUser(authData: { email: string, password: string }): Promise<IUserLoginResponse>;
+    abstract getListUsers(): Promise<IUserResponse[]>;
 }
